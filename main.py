85ba0d04e8ce348f2317ef527988e3fa7e68f03c
TheHighTecHacker® <thehightechacker@gmail.com>
2020-02-01 23:10:01 +0000

    i added more code to make the grid work

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import tkinter as tk
 from tkinter import messagebox
 
 class cube(object):
-  rows = 0
-  w = 0
+  rows = 20
+  w = 500
   def _init_(self,start,dirnx=1,dirny=0,color=(0, 255, 50 )):
     pass
-
-  def move(self, dirnx, dnry) :
+  def move(self, dirnx, dirny):
     pass
-
   def draw(self, surface, eyes=False):
     pass
 
@@ -29,35 +27,45 @@ class snake(object):
     pass
 
 def drawGrid(w, rows, surface):
-  pa
+  sizeBtwn = w // rows
+
+  x = 0
+  y = 0
+  for l in range(rows):
+    x = x + sizeBtwn
+    y = y + sizeBtwn
+    pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+    pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
 
 def redrawWindow(surface):
   global rows, width
-  win.fill((44, 88, 78 ))
+  surface.fill((44, 88, 78 ))
   drawGrid(width, rows, surface)
   pygame.display.update()
 
 def randomSnack(rows, items):
   pass
 
-  def messagebox(subject, content):
-    pass
+def messagebox(subject, content):
+  pass
 
 def main():
+  print("HI")
   global width, rows
   width = 500
   rows = 20
   win = pygame.display.set_mode((width, width))
-  s = snake ((44, 69, 88 ), (10,10))
-  flag = true
+ # s = snake((44, 69, 88 ), (10,10))
+  s = snake((255,0,0),(10,10))
+  flag = True
 
   clock = pygame.time.Clock()
   while flag:
     pygame.time.delay(50)
     clock.tick(10)
-
     redrawWindow(win)
   pass 
 
 
-  main()
+main()
+print("END")
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 https://www.youtube.com/watch?v=CD4qAhfFuLo
+8:40
+
+the grid was not showing up
