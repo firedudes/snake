87afbff2ff3464f955b2684a6efc9f323aea3e09
TheHighTecHacker® <thehightechacker@gmail.com>
2020-02-08 20:59:15 +0000

    added more code

--- main.py
+++ main.py
@@ -15,16 +15,65 @@ class cube(object):
     pass
 
 class snake(object):
-  def _init_(self, color, position):
-    pass
+  body = []
+  turns = {}
+
+  def _init_(self, color, pos):
+    self.color = color
+    self.head = cube(pos)
+    self.apped.append(self.head)
+    self = dirnx = 0
+    self = dirny = 1
   def move (self):
-    pass
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+
+    keys = pygame.key.get.pressed()
+
+    for keys in keys:
+        if keys[pygame.K_LEFT]:
+            self.dirnx = -1
+            self.dirny = 0
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        elif keys[pygame.K_RIGHT]:
+            self.dirnx = 1
+            self.dirny = 0
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        elif keys[pygame.K_UP]:
+            self.dirnx = 0
+            self.dirny = -1
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        elif keys[pygame.K_DOWN]:
+            self.dirnx = 0
+            self.dirny = 1
+            self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+    for i, c in enumerate(self.body):
+        p = c.pos[:]
+        if p in self.turns:
+            turn = self.turns[p]
+            c.move(turn[0], turn[1])
+            if i == len(self.body)-1 :
+                self.turns.pop(p)
+        else:
+          if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+          elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+          elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+          elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+          else: c.move(c.dirnx,c.dirny)
+       
+
   def reset(self, pos):
     pass
   def addCube(self):
     pass
   def draw(self, surface):
-    pass
+      for i, c in enumerate(self.body):
+        if i ==0:
+            c.draw(surface, True)
+        else:
+            c.draw(surface)
 
 def drawGrid(w, rows, surface):
   sizeBtwn = w // rows
@@ -50,13 +99,12 @@ def messagebox(subject, content):
   pass
 
 def main():
-  print("HI")
   global width, rows
   width = 500
   rows = 20
   win = pygame.display.set_mode((width, width))
  # s = snake((44, 69, 88 ), (10,10))
-  s = snake((255,0,0),(10,10))
+  #s = snake((255,0,0),(10,10))
   flag = True
 
   clock = pygame.time.Clock()
