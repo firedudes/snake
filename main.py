2a304c603937fce6a49c8c7f59e76062e132bffd
TheHighTecHacker® <thehightechacker@gmail.com>
2020-02-08 23:55:41 +0000

    added more code

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ class cube(object):
   rows = 20
   w = 500
   def _init_(self,start,dirnx=1,dirny=0,color=(0, 255, 50 )):
-    pass
+    self.pos = start
+    self.dirnx = 1
+    self.dirny = 0 
+    self.color = color 
   def move(self, dirnx, dirny):
-    pass
+    self.dirnx = dirnx
+    self.dirny = dirny
+    self.pos(self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
   def draw(self, surface, eyes=False):
-    pass
-
+    dis = self.w // self.rows
+    i = self.pos[0]
+    j =  self.pos[1]
+    pygame.draw.rect(surface, self.color (i*dis+1,j*dis+1, dis-2, dis-2))
 class snake(object):
   body = []
   turns = {}
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=CD4qAhfFuLo
 8:40
-
+30:30
 the grid was not showing up
